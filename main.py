cbe9d2bd6dd7e3478056118fdae8213617bd5279
David2022-art <bedoyacarmonajuandavid@gamil.com>
2026-03-12 16:09:27 -0500

    creacion de login o validacion de credenciales

---
+++ main.py
@@ -0,0 +1,17 @@
+print("Caleb Mora y David Bedoya")
+print("Caleb Mora y David Bedoya")
+print("Caleb Mora y David Bedoya")
+print("Caleb Mora y David Bedoya")
+
+name = input()
+print(name)
+
+user = "admin@uniremington.com"
+pasword = "admin123"
+user_input = input()
+pasword_input = input()
+
+if user_input== user and pasword_input== pasword:
+    print("bienvenido al sistema")
+else:
+    print("usuarion no encontrado")
